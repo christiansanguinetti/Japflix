f84509bd4f8e49af717f2bbd9961e57fc76bfb8a
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-09-21 20:15:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const japflix = " https://japceibal.github.io/japflix_api/movies-data.json"
-const boton = document.getElementById ("btnBuscar")
-const busqueda = document.getElementById ("inputBuscar")
+const boton = document.getElementById("btnBuscar")
+const busqueda = document.getElementById("inputBuscar")
 
 
 async function getData() {
@@ -8,16 +8,20 @@ async function getData() {
     data = await data.json()
     arreglo = data
     //console.log(arreglo)
-    boton.addEventListener('click',() => {
-       })       
+    boton.addEventListener('click', () => {
+    })
 }
 
-function buscar (arr){
- if (busqueda.value){
-  for( let peli of arr){
-    if (peli.title.include(busqueda.value) || peli.tagline.include(busqueda.value)  || peli.overview.include(busqueda.value))
-  }  
- }
+function buscar(arr) {
+    if (busqueda.value) {
+        for (let peli of arr) {
+            if (peli.title.include(busqueda.value) ||
+                peli.tagline.include(busqueda.value) ||
+                peli.overview.include(busqueda.value)){
+                    
+                }
+  }
+    }
 }
 
 ///document.addEventListener("DOMContentLoaded",() =>{
