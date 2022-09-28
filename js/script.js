80d5d5ec543e663b50be7ab51f36b47265574b1c
camilaabarno <camilabarno@hotmail.com>
2022-09-28 18:15:26 -0300

    Update script.js
    a

--- js/script.js
+++ js/script.js
@@ -1,31 +1,64 @@
-const japflix = " https://japceibal.github.io/japflix_api/movies-data.json"
+const japflix = "https://japceibal.github.io/japflix_api/movies-data.json"
 const boton = document.getElementById("btnBuscar")
 const busqueda = document.getElementById("inputBuscar")
 const contenedor = document.getElementById("lista")
 
-async function getData() {
-    let data = await fetch(japflix)
-    data = await data.json()
-    arreglo = data
-    //console.log(arreglo)
-    boton.addEventListener('click', () => {
-    buscar(arr)
-    })
-}
 
-function buscar(arr) {
+function cargar(arr) {
+        for (let peli of arr) {
+             console.log(peli.title);  
+                }
+}
+  
+function buscar(arr){
+    contenedor.innerHTML = "";
     if (busqueda.value) {
         for (let peli of arr) {
-            if (peli.title.include(busqueda.value) ||
-                peli.tagline.include(busqueda.value) ||
-                peli.overview.include(busqueda.value)) {
-                for (let genero of peli) {
-                    genero.name == busqueda.value
-                }
-            }
-        }
-    } 
-}
+      if ( peli.title.toLowerCase().includes(busqueda.value.toLowerCase()) ||
+      peli.tagline.toLowerCase().includes(busqueda.value.toLowerCase()) ||
+      peli.overview.toLowerCase().includes(busqueda.value.toLowerCase())
+      ){
+        contenedor.innerHTML += `<div><p style="color: white;">${peli.title}</p></div>`;
+        console.log(peli.title);
+        }}}}
+       // busqueda.value.toLowerCase().includes(peli.title.toLowerCase()) 
+    //    || 
+    //   busqueda.value.toLowerCase().includes(peli.tagline.toLowerCase()) ||
+    //   busqueda.value.toLowerCase().includes(peli. overview.toLowerCase())
+     
+    //              for (let genero of peli.genres) {
+    //                 if(genero.name.toLowerCase() == busqueda.value.toLowerCase()){
+    //                     console.log(peli.title);
+    //                 }
+                    
+    //              }
+    //         }
+    //     }
+    // }
+
+    
+
+   
+
+    
+fetch(japflix).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    cargar(data);
+    boton.addEventListener('click', () => {
+        buscar(data);
+       })
+});
+
+// async function getData() {
+//     let data = await fetch(japflix)
+//     data = await data.json()
+//     arreglo = data
+//     //console.log(arreglo)
+
+// }
+
+
 
 function showData(){
    for (pelicula of arr)
