50e1833fd9b85fd994f461caeaa484f0a6262869
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-09-30 17:55:23 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -24,39 +24,19 @@ function buscar(arr) {
                 peli.overview.toLowerCase().includes(busqueda.value.toLowerCase())
                 //peli_genero.toLowerCase().includes(busqueda.value.toLowerCase())
             ) {
-                contenedor.innerHTML += `<br> <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"><div style="color: white;">${peli.title} ${showStars(peli.vote_average/2)} </h4> <p style="color: white;">${peli.tagline}</p></div></button>
+                contenedor.innerHTML += `<br> <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"><div style="color: white;" onClick ="id(peli)">${peli.title} ${showStars(peli.vote_average/2)} </h4> <p style="color: white;">${peli.tagline}</p></div></button>
 
-                <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-                  <div class="offcanvas-header">
-                    <div> <h5 class="offcanvas-title" id="offcanvasTopLabel">${peli.title}</h5> </div> <br>
-                    <div><p>${peli.overview}</p> </div>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                  </div>
-                  <div class="offcanvas-body">
-                  </div>
-                </div>
                 `;
                 console.log(peli.title);
             }
         }
     }
 }
-function menu(){
-    contenedor.innerHTML += ` 
-    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
 
-<div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-  </div>
-</div>
-    `
-    alert("funciono" )
+const pongo_titulo = document.getElementById("offcanvasTopLabel") 
+function id (peli){
+    localStorage.setItem("peliid", peli.id);
 }
-
 
 
 fetch(japflix).then(function (response) {
